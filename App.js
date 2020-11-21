660e97b1f9fc9732de65f336b09817fb6386eb9a
jaredjames2020 <jaredjames2020@gmail.com>
2020-11-20 20:48:03 -0500

    test

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home.jsx'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your project!</Text>
       <Text>Now what!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Text>Show me what you got!</Text>
+            </div>
+        );
+    }
+}
+
+export default Home;
